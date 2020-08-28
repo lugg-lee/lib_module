69719239c7dfc316dab586e2171e2408b1518429
lugg-lee <salt-fish-17@outlook.com>
2020-08-28 21:40:23 +0800

    utils
    Change-Id: Ia1624db17df7885cccc5018aa17054324926cb82

---
+++ src/main/java/com/lugg/mylib/utils.kt
@@ -0,0 +1,14 @@
+package com.lugg.mylib
+
+import android.util.Log
+
+/**
+ * Created by Archer on 2020/8/28.
+ */
+
+object Utils {
+
+    fun log(text: String) {
+        Log.i("lugg", text)
+    }
+}
